9b21b44a6a13fa26bf53ea6b8f7dbeed9ce09026
Madison Evans <m.corbinevans@gmail.com>
2023-01-18 08:24:16 -0700

    commit tree 0860f982ee97e9f823e0e5bce61fd19f59b85836

--- src/index.js
+++ src/index.js
@@ -6,16 +6,25 @@ TODO:
 2. render ramen to the dom 
 3. add event listener to ramenImg and console log details once clicked 
 4. write a helper function "populateDetail" which populates the details div 
+5. add form input and put data into menu (does not have to persist)
 */
 
 document.addEventListener("DOMContentLoaded", init);
-const imgSection = document.getElementById("ramen-menu");
+
+//constants from DOM
+const ramenMenu = document.getElementById("ramen-menu");
 const ramenDetail = document.getElementById("ramen-detail");
 const ramenDetailImg = document.querySelector("#ramen-detail img");
 const ramenDetailName = document.querySelector("#ramen-detail h2");
 const ramenDetailRestaurant = document.querySelector("#ramen-detail h3");
 const ramenDetailRating = document.getElementById("rating-display");
 const ramenComment = document.getElementById("comment-display");
+const ramenForm = document.querySelector("form");
+const inputName = document.getElementById("new-name");
+const inputRestaurant = document.getElementById("new-restaurant");
+const inputImg = document.getElementById("new-image");
+const inputRating = document.getElementById("new-rating");
+const inputComment = document.getElementById("new-comment");
 
 // render function
 function renderRamen(ramenObj) {
@@ -24,8 +33,31 @@ function renderRamen(ramenObj) {
   ramenImg.addEventListener("click", () => {
     populateDetail(ramenObj);
   });
-  imgSection.appendChild(ramenImg);
+  ramenMenu.appendChild(ramenImg);
 }
+
+//form submission
+ramenForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("form submitted");
+
+  console.log(
+    inputComment.value,
+    inputImg.value,
+    inputName.value,
+    inputRating.value,
+    inputRestaurant.value
+  );
+
+  renderRamen({
+    id: 6,
+    name: inputName.value,
+    restaurant: inputRestaurant.value,
+    image: inputImg.value,
+    rating: inputRating.value,
+    comment: inputComment.value,
+  });
+});
 
 //populate details on click
 function populateDetail(ramenObj) {
@@ -39,8 +71,6 @@ function populateDetail(ramenObj) {
 
 // on load function (pulling data from server)
 function init() {
-  console.log("test");
-
   fetch("http://localhost:3000/ramens")
     .then((res) => res.json())
     .then((ramenObjArray) => {
